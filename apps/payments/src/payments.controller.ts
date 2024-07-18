bcaac2aafc753e30891cc381abfa479498eee9b3
Or Hasson <hassonor@gmail.com>
2024-07-18 19:25:43 +0300

    setup stripe on payments microservice

---
+++ apps/payments/src/payments.controller.ts
@@ -0,0 +1,17 @@
+import { Controller } from "@nestjs/common";
+import { PaymentsService } from "./payments.service";
+import { MessagePattern, Payload } from "@nestjs/microservices";
+import { CreateChargeDto } from "../dto/create-charge.dto";
+
+@Controller()
+export class PaymentsController {
+  constructor(private readonly paymentsService: PaymentsService) {
+  }
+
+  @MessagePattern("create_charge")
+  async createCharge(@Payload() data: CreateChargeDto) {
+    return this.paymentsService.createCharge(data);
+  }
+
+
+}
---
+++ apps/payments/src/payments.module.ts
@@ -0,0 +1,22 @@
+import { Module } from "@nestjs/common";
+import { PaymentsController } from "./payments.controller";
+import { PaymentsService } from "./payments.service";
+import { ConfigModule } from "@nestjs/config";
+import { LoggerModule } from "@app/common";
+import * as Joi from "joi";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+        STRIPE_SECRET_KEY: Joi.string().required()
+      })
+    }),
+    LoggerModule],
+  controllers: [PaymentsController],
+  providers: [PaymentsService]
+})
+export class PaymentsModule {
+}
